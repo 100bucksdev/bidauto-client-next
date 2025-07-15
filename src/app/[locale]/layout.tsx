6ad2ccbab28db4511066113befc32c7541b22a6a
Jez-er <103408350+Jez-er@users.noreply.github.com>
2025-07-15 15:10:48 +0300

    Add i18n support and restructure app for locales
    Introduces English and Lithuanian locale JSON files, restructures app routing to support locale-based paths, and adds i18n utilities. Moves favicon and main page to locale-specific directories, updates global styles, and adds configuration and middleware for internationalization. Updates package files to reflect new dependencies and configuration.

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,35 @@
+import { DefaultMetadata } from '@/config/ceo.config'
+import { routing } from '@/i18n/routing'
+import type { Metadata } from 'next'
+import { hasLocale, NextIntlClientProvider } from 'next-intl'
+import { Roboto } from 'next/font/google'
+import { notFound } from 'next/navigation'
+import '../global.css'
+
+const roboto = Roboto({
+	variable: '--font-roboto-sans',
+	subsets: ['latin'],
+})
+
+export const metadata: Metadata = DefaultMetadata
+
+export default async function RootLayout({
+	children,
+	params,
+}: Readonly<{
+	children: React.ReactNode
+	params: Promise<{ locale: string }>
+}>) {
+	const { locale } = await params
+	if (!hasLocale(routing.locales, locale)) {
+		notFound()
+	}
+
+	return (
+		<html lang={locale}>
+			<body className={`${roboto.variable} antialiased`}>
+				<NextIntlClientProvider>{children}</NextIntlClientProvider>
+			</body>
+		</html>
+	)
+}
