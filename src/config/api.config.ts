40926b93f60e6c8184e342b6e33c8a49ac0da9ce
Jez-er <103408350+Jez-er@users.noreply.github.com>
2025-09-08 15:40:44 +0300

    fix

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,7 +1,8 @@
 import pfetch from '@astralis-team/primitive-fetch'
 
 export const $Api = pfetch.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1',
+	baseURL:
+		process.env.NEXT_PUBLIC_APP_API_URL || 'http://localhost:8000/api/v1',
 	headers: {
 		'Content-Type': 'application/json',
 	},
