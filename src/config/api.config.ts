68ac1b6efbbf9112a9bb389da16f27a89b1dadf9
Jez-er <103408350+Jez-er@users.noreply.github.com>
2025-09-16 19:28:41 +0300

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -4,7 +4,7 @@ const isServer = typeof window === 'undefined'
 
 export const $Api = pfetch.create({
 	baseURL: isServer
-		? process.env.API_URL_SERVER || 'http://localhost:8000/api/v1'
+		? process.env.API_URL_SERVER || 'http://host.docker.internal:8000/api/v1'
 		: process.env.NEXT_PUBLIC_API_URL_CLIENT || 'http://localhost:8000/api/v1',
 	headers: {
 		'Content-Type': 'application/json',
